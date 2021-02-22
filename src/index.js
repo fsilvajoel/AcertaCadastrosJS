2d80808a1ee0e5d8d424d55a55cfb42107a3a2dd
Joel Ferreira da Silva <jfsilva@inf.ufsm.br>
2021-02-21 22:40:06 -0300

    create route manager and init components of pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './index.css';
+import Home from './pages/Home';
+import Register from './pages/Register';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  <StrictMode>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
+  </StrictMode>,
+  document.getElementById('root')
 );
